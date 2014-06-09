127705af6ae7c0573d82f715a8b4b27514c6b849
azuma <kangaroonote@gmail.com>
2014-06-09 11:30:57 +0900

    Modify property name

--- SimpleAnimation.js
+++ SimpleAnimation.js
@@ -7,11 +7,11 @@
 var SimpleAnimation = function(arg) {
 	var flg       = true;
 	var roopcount = 0;//ループの階数
-	var pass      = arg.pass || 0;//始点,通過点1,通過点2,終点{startx,starty,p1x,p1y,p2x,p2y,endx,endy}
+	var path      = arg.path || 0;//始点,通過点1,通過点2,終点{startx,starty,p1x,p1y,p2x,p2y,endx,endy}
 	var angleRad  = arg.angle * Math.PI / 180;//方向
 	var speed     = arg.speed || 5;//速度
 	var friction  = arg.friction || 0;//摩擦
-	var point     = (pass.startx && pass.starty) ? { x:pass.startx, y:pass.starty } : { x:0, y:0 };//戻り値の座標軸
+	var point     = (path.startx && path.starty) ? { x:path.startx, y:path.starty } : { x:0, y:0 };//戻り値の座標軸
 	var callback  = arg.callback || undefined;//コールバック関数
 	var count     = 0;
 	var spriteObj = arg.sprite || 0;
@@ -27,15 +27,15 @@ var SimpleAnimation = function(arg) {
 	//*******************
 	//公開メソッド
 	return {
-	//------------------
-	//フィールドセット
+		//------------------
+		//フィールドセット
 		set : function(setarg) {
 			flg       = (setarg.flg) ? setarg.flg : flg;
 			roopcount = (setarg.roopcount) ? setarg.roopcount : roopcount;
-			pass      = (setarg.pass) ? setarg.pass : pass;
+			path      = (setarg.path) ? setarg.path : path;
 			speed     = (setarg.speed) ? setarg.speed : speed;
 			friction  = (setarg.friction) ? setarg.friction : friction;
-			point     = (setarg.start) ? { x:pass.startx,y:pass.starty } : point;
+			point     = (setarg.start) ? { x:path.startx,y:path.starty } : point;
 			callback  = (setarg.callback) ? setarg.callback : callback;
 			spriteObj.index = (setarg.spriteIndex) ? setarg.spriteIndex : spriteObj.index;
 		},
@@ -45,7 +45,7 @@ var SimpleAnimation = function(arg) {
 			var obj = {
 				flg       : flg,
 				roopcount : roopcount,
-				pass      : pass,
+				path      : path,
 				speed     : speed,
 				friction  : friction,
 				point     : point,
@@ -60,8 +60,8 @@ var SimpleAnimation = function(arg) {
 		//2地点間の移動
 		movePoint : function () {//開始,終了,摩擦,コールバック
 			 //距離を計算
-			var dx = pass.endx - pass.startx;
-			var dy = pass.endy - pass.starty;
+			var dx = path.endx - path.startx;
+			var dy = path.endy - path.starty;
 			var distance = Math.sqrt((dx*dx + dy*dy));
 			var step = Math.floor(distance / speed);
 			//ステップ数を繰り返す階数に代入
@@ -70,8 +70,8 @@ var SimpleAnimation = function(arg) {
 				flg = false;
 			}
 			//一度に移動する距離を算出
-			var xUnit = (pass.endx - pass.startx) / step;
-			var yUnit = (pass.endy - pass.starty) / step;
+			var xUnit = (path.endx - path.startx) / step;
+			var yUnit = (path.endy - path.starty) / step;
 			if(roopcount > 0) {
 				roopcount -= 1;
 				point.x += xUnit - (point.x * friction);
@@ -85,14 +85,14 @@ var SimpleAnimation = function(arg) {
 		//ベジェ移動
 		bezier : function () {//開始,終了,コールバック
 			var t  = count;
-			var cx = 3 * (pass.p1x - pass.startx);
-			var bx = 3 * (pass.p2x - pass.p1x) - cx;
-			var ax = pass.endx - pass.startx - cx - bx;
-			var cy = 3 * (pass.p1y - pass.starty);
-			var by = 3 * (pass.p2y - pass.p1y) - cy;
-			var ay = pass.endy - pass.starty - cy - by;
-			point.x = ax*(t*t*t) + bx*(t*t) + cx*t + pass.startx;
-			point.y = ay*(t*t*t) + by*(t*t) + cy*t + pass.starty;
+			var cx = 3 * (path.p1x - path.startx);
+			var bx = 3 * (path.p2x - path.p1x) - cx;
+			var ax = path.endx - path.startx - cx - bx;
+			var cy = 3 * (path.p1y - path.starty);
+			var by = 3 * (path.p2y - path.p1y) - cy;
+			var ay = path.endy - path.starty - cy - by;
+			point.x = ax*(t*t*t) + bx*(t*t) + cx*t + path.startx;
+			point.y = ay*(t*t*t) + by*(t*t) + cy*t + path.starty;
 			count += speed;
 			if(count > 1 && !callback){
 				count = 1;
@@ -108,9 +108,8 @@ var SimpleAnimation = function(arg) {
 		sprite : function () {//画像のインデックス,画像の幅,画像の高さ,切抜の幅,切抜の高さ}
 			spriteObj.x = (spriteObj.index % spriteObj.row) * spriteObj.peekWidth;
 			spriteObj.y = Math.floor(spriteObj.index / spriteObj.col) * spriteObj.peekHeight;
-			spriteObj.position = {x:spriteObj.x,y:spriteObj.y,max:spriteObj.maxIndex};
+			spriteObj.position = {x:spriteObj.x, y:spriteObj.y, max:spriteObj.maxIndex};
 			return spriteObj.position;
 		}
 	};
 };
-
